7ad45cb9d6b3ddbc0fdee6ca28dca6270a4cc3e6
Lê Trần Trung Hiếu <trunghieumickey@outlook.com.vn>
2023-12-02 08:40:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 // Import Three.js from CDN
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.159.0/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.159.0/build/three.module.min.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.159.0/examples/jsm/controls/OrbitControls.js';
 
-// Create a scene`
+// Create a scene
 const scene = new THREE.Scene();
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.set(0, 5, 10);
+camera.lookAt(scene.position);
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Create control
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// Create a light
-const light = new THREE.PointLight(0xffffff, 1);
-light.position.set(0, 0, 5);
-scene.add(light);
+// Create a ground
+const groundGeometry = new THREE.PlaneGeometry(10, 10);
+const groundMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2;
+scene.add(ground);
+
+// Create sunlight as a directional light
+const sunlight = new THREE.DirectionalLight(0xffffff, 1);
+sunlight.position.set(1, 1, 1);
+scene.add(sunlight);
 
 // Render the scene
 function animate() {
