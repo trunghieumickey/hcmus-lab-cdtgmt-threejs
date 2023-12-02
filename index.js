fb3d700e3166c0506974073b50b5bbd26fe55e06
Lê Trần Trung Hiếu <trunghieumickey@outlook.com.vn>
2023-12-02 08:48:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ scene.add(sunlight);
 // Render the scene
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
