69a2a4f3d5b4dec1dbc69fa2e59155c17355259e
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-12-03 19:11:38 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 5, 10);
+camera.position.set(-100, 100, 100);
 camera.lookAt(scene.position);
 
 // Create a renderer
@@ -18,11 +18,17 @@ document.body.appendChild(renderer.domElement);
 // Create control
 const controls = new OrbitControls(camera, renderer.domElement);
 
+//texture loader
+const textureLoader = new THREE.TextureLoader();
+
 // Create a ground
-const groundGeometry = new THREE.PlaneGeometry(10, 10);
-const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const groundGeometry = new THREE.PlaneGeometry(200, 200);
+const sandTexture = textureLoader.load('./texture/desertsand.jpg');
+const groundMaterial = new THREE.MeshBasicMaterial({ map: sandTexture });
 const ground = new THREE.Mesh(groundGeometry, groundMaterial);
 ground.rotation.x = -Math.PI / 2;
+//rotate z 45 degree
+ground.rotation.z = Math.PI / 4;  
 scene.add(ground);
 
 // Create sunlight as a directional light
@@ -35,6 +41,20 @@ const skyboxGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
 const skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x87ceeb, side: THREE.BackSide });
 const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 scene.add(skybox);
+
+// Create pyramid
+const pyramidTexture = textureLoader.load('./texture/sandstone.jpg');
+const pyramidMaterial = new THREE.MeshBasicMaterial({ map: pyramidTexture });
+function createPyramid(side, height, west, south) {
+  const pyramidGeometry = new THREE.ConeGeometry(side / 2, height, 4);
+  const pyramid = new THREE.Mesh(pyramidGeometry, pyramidMaterial);
+  pyramid.position.set(west-50, height / 2, south-50);
+  scene.add(pyramid);
+}
+
+createPyramid(23, 14.66, 0, 0); // Khufu pyramid
+createPyramid(21.5, 13.65, 21.5, 44.8); // Khafre pyramid
+createPyramid(10.85, 6.65, 54.4, 109.6); // Menkaure pyramid
 
 // Render the scene
 function animate() {
